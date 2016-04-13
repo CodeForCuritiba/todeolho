27ea247587fe78f969f3060de9066b194f29adfa
gustavomagalhaes <gustavo.magalhaes86@gmail.com>
2016-04-13 20:13:13 -0300

    Inclusão da dialog

--- app/src/main/java/br/com/todeolho/todeolho/AmigosFragment.java
+++ app/src/main/java/br/com/todeolho/todeolho/AmigosFragment.java
@@ -2,12 +2,10 @@ package br.com.todeolho.todeolho;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 /**
  * Created by gustavomagalhaes on 4/11/16.
@@ -20,6 +18,17 @@ public class AmigosFragment extends Fragment{
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_amigos, container, false);
 
+        Button btn = (Button) rootView.findViewById(R.id.btnTeste);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MarkerFragmentDialog dialog = new MarkerFragmentDialog();
+                dialog.show(getActivity().getSupportFragmentManager(), "teste");
+            }
+        });
+
         return rootView;
     }
+
 }
